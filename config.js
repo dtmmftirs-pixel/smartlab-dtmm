2dfa0ff910ad0eecc24efce6d991ae3f5a1b0417
dtmmftirs-pixel <dtmm.ftirs@gmail.com>
2026-07-22 23:06:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // KONFIGURASI - GANTI DENGAN DATA ANDA
 
 // URL Web App Google Apps Script Anda
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwcPRVlRopuG_KQwj6wQfioURmURLr75lVCjVUVrAMvjIRpcBnJi1ISY_mwRa7qEEWK/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwK3rU5PgWM3oO1uK8GfMeO_qgAgzggRkK8vK1SXJR4fYIVNaFAttsdLrUpn_jlgp5Rrw/exec';
 
 // Client ID Google OAuth Anda
 const GOOGLE_CLIENT_ID = '96706539653-e1h205ej3ne8uhmehl6nfe4bdhgkimm2.apps.googleusercontent.com';
